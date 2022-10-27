247020992ed3f5a5a8cad710724ee3557244c0c7
GTEgorss <noartu@gmail.com>
2022-10-27 23:46:26 +0300

    fix: fix for nav bar: wrap, scroll and other adjustments

--- JS/ScrollMod.js
+++ JS/ScrollMod.js
@@ -1,25 +1,36 @@
-let lastKnownScrollPosition = 0;
+let lastScrollY = 0;
+let lastWidth = window.innerWidth;
+let maxScrollPos = 105;
 let ticking = false;
 let fixed = false;
 
-function modifyTitle(scrollPos) {
+function modifyTitle(scrollPos, width) {
 
-    if (scrollPos > 107 && !fixed) {
+
+    if (width > 773) {
+        maxScrollPos = 105;
+    } else {
+        maxScrollPos = 164;
+    }
+
+    console.log(scrollPos + " " + maxScrollPos + " " + width + "\n");
+
+    if (scrollPos > maxScrollPos && !fixed) {
         const topNav = document.querySelector('.top-nav');
         const main = document.querySelector('main');
 
         topNav.style.position = "fixed";
-        topNav.style.padding = "30px 0 20px";
-        main.style.paddingTop = "40px";
+        topNav.style.padding = "23px 0 0";
+        main.style.paddingTop = "70px";
 
         fixed = true;
 
-    } else if (scrollPos <= 107 && fixed) {
+    } else if (scrollPos <= maxScrollPos && fixed) {
         const topNav = document.querySelector('.top-nav');
         const main = document.querySelector('main');
 
         topNav.style.position = "relative";
-        topNav.style.padding = "0 0 20px";
+        topNav.style.padding = "10px 0 0";
         main.style.paddingTop = "0";
 
         fixed = false;
@@ -27,11 +38,13 @@ function modifyTitle(scrollPos) {
 }
 
 document.addEventListener('scroll', (e) => {
-    lastKnownScrollPosition = window.scrollY;
+    lastScrollY = window.scrollY;
+    lastWidth = window.innerWidth;
+
 
     if (!ticking) {
         window.requestAnimationFrame(() => {
-            modifyTitle(lastKnownScrollPosition);
+            modifyTitle(lastScrollY, lastWidth);
             ticking = false;
         });
 
