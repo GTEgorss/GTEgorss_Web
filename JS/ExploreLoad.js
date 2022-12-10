e9a569e8cb473fc450373d9cd0ab29ea34d75ae8
GTEgorss <noartu@gmail.com>
2022-12-10 20:52:43 +0300

    feat: fixed main margin chore: initial explore fetch API query

--- JS/ExploreLoad.js
+++ JS/ExploreLoad.js
@@ -1,3 +1,5 @@
 (() => {
-
+    fetch('https://my-json-server.typicode.com/GTEgorss/GTEgorss_Web/playlists')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 })();
